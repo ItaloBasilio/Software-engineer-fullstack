4dc1c65dfe72b42ef4a6be697ee36cf342f2a0f2
ItaloBasilio <italobasiliodasilva@gmail.com>
2024-03-24 20:53:10 -0300

    ORM - sequelize

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'fullstack',
+    database: 'fullstack',
+    define: {
+        timespamps: true,
+        underscored: true,
+        underscoredAll: true,
+    },
+}
